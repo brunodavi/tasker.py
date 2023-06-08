aafc5b844871e9340a846554566ba65b3473c78e
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-06-08 18:02:19 -0300

    refactor: alterando ações feitas com eval

--- tasker/py/action.py
+++ tasker/py/action.py
@@ -1,72 +1,59 @@
 from re import match
 
 class Action:
-    _NEW_INIT = (
-        'lambda self, {params}: '
-        '[ '
-            'setattr(self, k, v) '
-            'for k, v in locals().items() '
-            'if self != v and v is not None '
-        '] and None or None'
-    )
-
     _is_context_creator = False
 
 
     def __init__(self, *args, **kwargs):
         cls = self.__class__
-        params = self.__get_params(cls)
 
-        if not len(params):
+        props, max_props = self.__get_props(cls)
+        max_args = len(args)
+
+        if max_args > max_props:
+            raise IndexError(
+                f"{max_args} passou do limite "
+                f"de propriedades ({max_props})"
+            )
+
+        for index, arg in enumerate(args):
+            key = props[index]
+            setattr(self, key, arg)
+
+        for index, key in enumerate(kwargs):
+            if key in props:
+                arg = kwargs[key]
+                setattr(self, key, arg)
+            else:
+                raise KeyError(
+                    f"'{key}' não existe como "
+                    "propriedade dessa ação."
+                )
+
+
+    def __get_props(self, cls):
+        props = []
+
+        all_props = vars(cls).keys()
+        all_props = tuple(all_props)
+        all_props = all_props[1:-1]
+
+        for key in all_props:
+            if self.__is_static(key):
+                props.append(key)
+            else:
+                delattr(cls, key)
+
+        max_props = len(props)
+
+        if not max_props:
             raise AttributeError(
                 'Nenhum parametro encontrado, '
                 'defina um parâmetro na classe: '
                 f'{cls.__name__}.'
             )
 
-        cls.__init__ = self.__create_init(params)
-
-    def __call__(self):
-        if self._is_context_creator:
-            return self.create_action()
-
-        return self.take_action()
-
-    def __create_init(self, params):
-        serialized_params = (
-            self
-            .__serialize_params(params)
-        )
-
-        code = (
-            self
-            ._NEW_INIT
-            .format(params=serialized_params)
-        )
-
-        new_init = eval(code, {}, {})
-        new_init.__name__ = '__init__'
-
-        return new_init
-
-    def __serialize_params(self, params):
-        params = map(
-            lambda key: f'{key}=None',
-            params,
-        )
-
-        params = ', '.join(params)
-
-        return params
-
-    def __get_params(self, cls):
-        result = []
-
-        for key in vars(cls).keys():
-            if self.__is_static(key):
-                result.append(key)
-
-        return result
+        return props, max_props
 
     def __is_static(self, attr_name: str):
         result = match(
@@ -81,8 +68,5 @@ class Action:
         return not_is_dunder_and_is_alphanum
 
 
-    def create_action(self):
-        return None
-
-    def take_action(self):
-        return None
+    def add_action(self):
+        ...
