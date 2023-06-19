89274bcde923e6521e33e9a5d8b079b9daa8436c
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-06-19 00:07:09 -0300

    feat: tarefas e ações implementadas

--- tasker/py/main.py
+++ tasker/py/main.py
@@ -1,10 +1,36 @@
+from dataclasses import dataclass, field
+
+from .client import TaskerPyClient
+
+from .profile import Profile
 from .task import Task
+from .scene import Scene
 
+
+@dataclass
 class TaskerPy:
-    def add_task(self, name):
-        def decorator(task_action):
-            task = Task(name)
-            task.__call__ = task_action
+    address: str = 'localhost'
+    port: int = 9170
 
+    client: TaskerPyClient | None = None
+
+    profiles: list[Profile] = field(default_factory=list)
+    tasks: list[Task] = field(default_factory=list)
+    scenea: list[Scene] = field(default_factory=list)
+
+
+    def __post_init__(self):
+        self.client = TaskerPyClient(
+            self.address,
+            self.port
+        )
+
+    def add_task(self, name: str):
+        def decorator(func):
+            task = Task(name)
+            func(task)
+
+            self.tasks.append(task)
             return task
+
         return decorator
--- tasker/py/task.py
+++ tasker/py/task.py
@@ -1,6 +1,29 @@
+from dataclasses import dataclass, field
+from typing import Any
+
+from .action import Action
+from ..task_collision import TaskCollision
+
+@dataclass
 class Task:
-    def __init__(self, name):
-        self.name = name
+    name: str
+
+    collision: TaskCollision = (
+        TaskCollision.ABORT_NEW_TASK
+    )
+
+    par1: Any = None
+    par2: Any = None
+
+    variables: dict[str, Any] = field(default_factory=dict)
+
+    actions: list[Action] = field(default_factory=list)
+
 
     def __call__(self, *args, **kwargs):
-        pass
+        ...
+
+
+    def add_action(self, action: Action):
+        self.actions.append(action)
+
