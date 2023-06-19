aa3f5ad5b6576a61f472c766898628c77da56902
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-06-19 00:03:47 -0300

    refactor: cliente movido

---
+++ tasker/py/client.py
@@ -0,0 +1,14 @@
+from httpx import Client
+
+
+class TaskerPyClient(Client):
+    def __init__(
+            self,
+            address = 'localhost',
+            port = 9170,
+        ):
+
+        super().__init__(
+            base_url=f'http://{address}:{port}',
+            timeout=30,
+        )
