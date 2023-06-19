a30c4fe372fd9903f20c333c79da584c512911cd
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-06-19 00:08:51 -0300

    test: criação de ações e tarefas

--- test/test_tasker.py
+++ test/test_tasker.py
@@ -2,14 +2,21 @@ from tasker.py import TaskerPy, Task
 from tasker.actions.alert import Beep
 
 
-def test_criar_tarefa():
+def test_criar_tarefa_e_ações():
     app = TaskerPy()
-    beep = Beep()
 
     @app.add_task('Task Name')
-    def task():
-        beep.add_action()
+    def task(t: Task):
+        beep = Beep(1000, duration=100)
+        t.add_action(beep)
+
+        for _ in range(3):
+            beep.frequency += 1000
+            t.add_action(beep)
+
 
     assert isinstance(task, Task)
     assert task.name == 'Task Name'
 
+    assert len(task.actions) == 4
+    assert len(app.tasks) == 1
