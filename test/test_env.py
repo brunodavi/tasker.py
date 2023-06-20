3251d989d4c8e6fe3d2ecc5ccb009098d4ca1bf2
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-06-19 22:14:06 -0300

    test: da classe Env

---
+++ test/test_env.py
@@ -0,0 +1,16 @@
+from pytest import MonkeyPatch
+
+from tasker.env import Env
+
+
+def test_TASKER_PY_PACKAGE_é_uma_variavel_de_ambiente(
+        monkeypatch: MonkeyPatch
+    ):
+
+    monkeypatch.setenv(
+        'TASKER_PY_PACKAGE',
+        'com.taskerpy'
+    )
+
+
+    assert Env().TASKER_PY_PACKAGE == 'com.taskerpy'
