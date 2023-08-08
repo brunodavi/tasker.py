5d3cdc1c51392cc5ec47a264a5deee105879402f
brunodavi <brunodaviandrade2000@gmail.com>
2023-08-08 00:03:25 -0300

    test: xml_utils na ação beep

--- test/test_xml_utils.py
+++ test/test_xml_utils.py
@@ -7,16 +7,19 @@ from tasker.actions.alert import Beep
 TASK_XML = (
 """
 <TaskerData sr="" dvi="1">
-	<Task sr="task0">
-		<id>0</id>
+	<Task sr="task1">
+		<cdate>0</cdate>
+		<edate>1</edate>
+		<id>1</id>
 		<nme>task_test</nme>
+		<pri>100</pri>
 		<Action sr="act0" ve="7">
 			<code>171</code>
 			<Int sr="arg0" val="8000"/>
 			<Int sr="arg1" val="1000"/>
 			<Int sr="arg2" val="50"/>
 			<Int sr="arg3" val="3"/>
-			<Str sr="arg4" ve="3"/>
+			<Str sr="arg4" val=""/>
 		</Action>
 	</Task>
 </TaskerData>
@@ -33,7 +36,7 @@ def test_task_xml_creator():
     xml_task = (
         xml_utils
             .create_task(
-                0, 'task_test',
+                1, 'task_test',
                 Beep()
             )
     )
