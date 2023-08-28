fb4a0ccf7d114df0e61f00d803dbf0b43ab1441f
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-08-27 22:25:56 -0300

    feat: exportação e execução das tarefas

--- src/tasker/py/main.py
+++ src/tasker/py/main.py
@@ -1,13 +1,17 @@
-from dataclasses import dataclass, field
+from os import getenv
+from random import randint
+from dataclasses import dataclass
 
 from .client import TaskerPyClient
+
+from .profile import Profile
 from .task import Task
 from .scene import Scene
 
 
 @dataclass
 class TaskerPy:
-    address: str = 'localhost'
+    address: str = getenv('TASKER_PY_ADDRESS') or 'localhost'
     port: int = 9170
 
     def __post_init__(self):
@@ -16,16 +20,38 @@ class TaskerPy:
             self.port
         )
 
-        self._profiles = []
-        self._tasks = []
-        self._scenes = []
+        self._profiles: list[Profile] = []
+        self._tasks: list[Task] = []
+        self._scenes: list[Scene] = []
+
+    def _generate_id(
+            self,
+            items: list[Profile] | list[Task] | list[Scene],
+            randint_args = [1_000, 100_000],
+        ) -> int:
+        new_id = randint(*randint_args)
+
+        if any((new_id == item.id for item in items)):
+            return self._generate_id(items, randint_args)
+        
+        return new_id
 
 
-    def add_task(self, name: str | None = None):
+    def add_task(
+            self,
+            name: str | None = None,
+            task_id: int = None,
+            returned: dict[str, str] = None
+        ):
+        if task_id is None: task_id = self._generate_id(self._tasks)
+
         def decorator(action_func):
             task = Task(
+                task_id,
                 name or action_func.__name__,
-                action_func
+                action_func,
+                returned,
+                self._client,
             )
 
             self._tasks.append(task)
--- src/tasker/py/task.py
+++ src/tasker/py/task.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Any, Callable, Generator
@@ -9,6 +8,7 @@ from lxml import etree
 from ..xml_utils import XmlUtils
 from .action import Action
 from ..task_collision import TaskCollision
+from .client import TaskerPyClient
 
 
 LOCAL_TASKS = '/sdcard/Tasker/tasks/'
@@ -16,8 +16,12 @@ LOCAL_TASKS = '/sdcard/Tasker/tasks/'
 
 @dataclass
 class Task:
+    id: int
     name: str
-    actions: Callable[..., Generator[Action, None, None]]
+
+    _actions: Callable[..., Generator[Action, None, None]]
+    _returned: dict[str, str]
+    _client: TaskerPyClient
 
     priority: int = 100
     collision: TaskCollision = (
@@ -31,23 +35,42 @@ class Task:
 
 
     def __call__(self, *args, **kwargs):
-        for action in self.actions(self):
+        for action in self._actions(self):
             yield deepcopy(action)
 
     def to_string(self):
         xml_utils = XmlUtils()
 
         xml_task = xml_utils.create_task(
+            self.id,
             self.name,
             *self()
         )
 
-        return etree.tostring(xml_task)
+        return etree.tostring(xml_task).decode()
 
-    def export(self, diretory = LOCAL_TASKS):
+    def export(self, directory: str | Path = LOCAL_TASKS):
         filepath = (
-            Path(diretory) / f'{self.name}.tsk.xml'
+            Path(directory) / f'{self.name}.tsk.xml'
         )
 
         xml_string = self.to_string()
-        return filepath.write_text(xml_string)
+        filepath.write_text(xml_string)
+        return filepath
+
+    def play(self):
+        body = {
+            'task_xml': self.to_string(),
+
+            'task_name': self.name,
+            'par1': self.par1,
+            'par2': self.par2,
+
+            'response_schema': {
+                'status_code': 200,
+                'headers': 'host:tasker.py',
+                'body': self._returned
+            }
+        }
+
+        return self._client.post('/run', json=body).json()
