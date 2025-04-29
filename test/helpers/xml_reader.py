e88b56018060b59f49d9541b151225ef39e9a219
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2025-04-29 20:23:26 -0300

    refactor: tests with helper to read xml and mocks organization

---
+++ test/helpers/xml_reader.py
@@ -0,0 +1,27 @@
+from lxml import etree
+
+
+MOCK_DIR = 'test/__mocks__'
+
+
+def xml_to_string(element_xml):
+    etree.tostring(element_xml, pretty_print=True, encoding='utf-8').decode()
+
+
+def read_xml(path):
+    parser = etree.XMLParser(remove_blank_text=True)
+    mock_tree = etree.parse(path, parser)
+
+    return xml_to_string(mock_tree)
+
+
+def read_xml_task(name):
+    return read_xml(f'{MOCK_DIR}/tasks/{name}')
+
+
+def read_xml_profile(name):
+    return read_xml(f'{MOCK_DIR}/profiles/{name}')
+
+
+def read_xml_scene(name):
+    return read_xml(f'{MOCK_DIR}/scenes/{name}')
--- test/test_task_xml.py
+++ test/test_task_xml.py
@@ -1,23 +1,16 @@
-from lxml import etree
 from pytest import fixture
 
 from tasker.py.profile_variable import ProfileVariable
 from tasker.xml import TaskerXml
 
-
-def xml_to_string(element_xml):
-    etree.tostring(element_xml, pretty_print=True, encoding='utf-8').decode()
-
-
-def read_xml(path):
-    parser = etree.XMLParser(remove_blank_text=True)
-    mock_tree = etree.parse(path, parser)
-
-    return xml_to_string(mock_tree)
+from test.helpers.xml_reader import (
+    read_xml_task,
+    xml_to_string
+)
 
 
 def test_beep_xml(beep_task):
-    expected_xml = read_xml('test/__mocks__/beep.xml')
+    expected_xml = read_xml_task('beep.xml')
 
     task_xml = TaskerXml(tasks=[beep_task]).to_xml()
     xml_string = xml_to_string(task_xml)
@@ -25,7 +18,7 @@ def test_beep_xml(beep_task):
 
 
 def test_flash_xml(flash_task):
-    expected_xml = read_xml('test/__mocks__/flash.xml')
+    expected_xml = read_xml_task('flash.xml')
 
     task_xml = TaskerXml(tasks=[flash_task]).to_xml()
     xml_string = xml_to_string(task_xml)
@@ -33,7 +26,7 @@ def test_flash_xml(flash_task):
 
 
 def test_task_profile_variable(flash_task):
-    expected_xml = read_xml('test/__mocks__/flash_profile_variables.xml')
+    expected_xml = read_xml_task('flash_profile_variables.xml')
 
     flash_task.profile_variables = [
         ProfileVariable(variable_name='%test_variable', value='TEST_VALUE'),
